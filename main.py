9fc56c48a18b934de9bc2487c55cb7d5b6dfe7d4
Pancham <pancham1603@gmail.com>
2021-01-15 03:22:00 +0530

    tokennew

--- main.py
+++ main.py
@@ -286,6 +286,6 @@ async def changepresence():
         await asyncio.sleep(3)
 
 
-token = ''  # input the unique bot token from dev panel (string)
+token = 'Nzk3NTM3NTAxNDQ2OTMwNDYz.X_n6rQ._rr60uvXTUsUv3rfv0tbYkhRpl0'  # input the unique bot token from dev panel (string)
 bot.loop.create_task(changepresence())  # to change status every 3 seconds
 bot.run(token)  # finally run the bot
